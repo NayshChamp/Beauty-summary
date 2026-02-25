65adf1ad729714ca6ce0d8c7a4d21b295b7b73fe
Reshrayz <reshrayz@gmail.com>
2026-02-25 12:43:22 +0300

    Externalize pg for SSR builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  ssr: {
+    external: [
+      'pg',            // Core Postgres driver
+      'pg-native',     // Optional native bindings
+      'pg-cloudflare'  // Cloudflare sockets variant
+    ]
+  },
   build: {
     rollupOptions: {
       external: [
